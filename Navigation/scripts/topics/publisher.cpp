b6f541738f165f45005fc5fc346235859b342cf0
RioMorll <al415556@uji.es>
2023-11-20 19:00:51 +0100

    Version 2.0.1 -- config sub

--- Navigation/scripts/topics/publisher.cpp
+++ Navigation/scripts/topics/publisher.cpp
@@ -1,13 +1,16 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/posestamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <iostream>
 
 using namespace std::chrono_literals;
 
-void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+geometry_msg::msg::Pose sub_pose;
+void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-  std::cout << msg->data << std::endl;
+ sub_pose = msg->pose;
 }
 
 int main(int argc, char * argv[])
@@ -15,7 +18,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("topic", 10);
-  auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
+  auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
   geometry_msgs::msg::PoseStamped message;
   rclcpp::WallRate loop_rate(500ms);
   int px1=1, py1=1, pz1=1, qx1=1, qy1=1, qz1=1, qw1=1;
