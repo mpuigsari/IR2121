e52e8a22fae21ce7b8212364f111fca3c701e775
RioMorll <al415556@uji.es>
2023-11-20 18:33:58 +0100

    Version 2.0 - Add subscriber

--- Navigation/scripts/topics/publisher.cpp
+++ Navigation/scripts/topics/publisher.cpp
@@ -1,16 +1,22 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/posestamped.hpp"
+#include <iostream>
 
 using namespace std::chrono_literals;
+
+void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+{
+  std::cout << msg->data << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("topic", 10);
+  auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
   geometry_msgs::msg::PoseStamped message;
-  auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
   int px1=1, py1=1, pz1=1, qx1=1, qy1=1, qz1=1, qw1=1;
   while (rclcpp::ok()) {
