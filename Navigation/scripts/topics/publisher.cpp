17352dd623272341f725579bc5655ccd8b1e2b44
RioMorll <al415556@uji.es>
2023-11-20 18:24:25 +0100

    Version 1.2 - Set Data

--- Navigation/scripts/topics/publisher.cpp
+++ Navigation/scripts/topics/publisher.cpp
@@ -12,9 +12,15 @@ int main(int argc, char * argv[])
   geometry_msgs::msg::PoseStamped message;
   auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
-
+  int px1=1, py1=1, pz1=1, qx1=1, qy1=1, qz1=1, qw1=1;
   while (rclcpp::ok()) {
-    message.data = "Hello, world! " + std::to_string(publish_count++);
+    message.pose.point.x = px1;
+    message.pose.point.y = py1;
+    message.pose.point.z = pz1;
+    message.pose.quaternion.x = qx1;
+    message.pose.quaternion.y = qy1;
+    message.pose.quaternion.z = qz1;
+    message.pose.quaternion.z = qw1;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
