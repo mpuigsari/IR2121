e4151852e05ef62d35f005cfd6eee1944b588de4
Max <al415556@uji.es>
2023-12-08 16:10:30 +0100

    Version 5.2  Create tf2 utilities

--- Navigation/nav/src/goal/publisher.cpp
+++ Navigation/nav/src/goal/publisher.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transformed_stamped.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 
@@ -18,13 +19,15 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
-  
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener(buffer);
+  geometry::msgs::TransformStamped map_baselink;
   geometry_msgs::msg::PoseStamped message;
   rclcpp::WallRate loop_rate(500ms);
+  
   double px[3]={-1,8.5,7.5}, py[3]={3,5,-2},pxi,pyi;
   int i=0;
   
-
   while (rclcpp::ok() & (i<3)) {
     message.pose.position.x = px[i];
     message.pose.position.y = py[i];
