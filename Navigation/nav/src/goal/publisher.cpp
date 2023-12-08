59bfabb116827032bd375a01789f1d825ee1221b
Max <al415556@uji.es>
2023-12-08 19:07:24 +0100

    Version 5.2.3 not detecting frames

--- Navigation/nav/src/goal/publisher.cpp
+++ Navigation/nav/src/goal/publisher.cpp
@@ -16,7 +16,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
- 
+  
   std::unique_ptr<tf2_ros::Buffer> buffer = std::make_unique<tf2_ros::Buffer>(node->get_clock());
   std::shared_ptr<tf2_ros::TransformListener> transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*buffer);
   
@@ -33,7 +33,7 @@ int main(int argc, char * argv[])
     message.pose.position.y = py[i];
     publisher->publish(message);
     
-    map_baselink = buffer->lookupTransform("base_link", "map", rclcpp::Time(0), rclcpp::Duration(1.0));
+    map_baselink = buffer->lookupTransform("base_link", "map", rclcpp::Time(0), rclcpp::Duration(2.0));
     tf2::doTransform(message, current, map_baselink);
     
     pxs=std::abs(current.pose.position.x);
