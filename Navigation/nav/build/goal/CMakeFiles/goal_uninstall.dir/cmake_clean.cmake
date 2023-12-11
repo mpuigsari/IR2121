01e028c6565be2261ba7cacc31f638bb76c7e233
RioMorll <104906009+RioMorll@users.noreply.github.com>
2023-12-11 19:23:50 +0100

    Delete Navigation/nav/build directory
    sobra

--- Navigation/nav/build/goal/CMakeFiles/goal_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/goal_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/goal_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Navigation/nav/build/goal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usuario/Documentos/GitHub/IR2121/Navigation/nav/src/goal
-# Build directory: /home/usuario/Documentos/GitHub/IR2121/Navigation/nav/build/goal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
